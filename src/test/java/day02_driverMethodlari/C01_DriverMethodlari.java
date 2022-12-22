06579e8808b3c287e57df9c59484c0dc488a2869
canertürk <canerturk19811981@gmail.com>
2022-12-22 23:56:13 +0300

    4.gün

--- src/test/java/day02_driverMethodlari/C01_DriverMethodlari.java
+++ src/test/java/day02_driverMethodlari/C01_DriverMethodlari.java
@@ -4,44 +4,32 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class C01_DriverMethodlari {
+    public C01_DriverMethodlari() {
+    }
 
     public static void main(String[] args) throws InterruptedException {
-
-        // Biz browser'lari olusturdugumuz driver sayesinde otomate edebiliyoruz
-        // Bunun icin her testin basinda mutlaka driver objesi olusturacagiz
-
-        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver_win32/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver_win32 (2)/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-
-        driver.get("https://amazon.com");
-        // acilan sayfanin basliginin amazon icerdigini test edin
-
+        driver.get("https://www.amazon.com");
         String expectedKelime = "amazon";
         String actualResult = driver.getTitle();
-
-        if (actualResult.contains(expectedKelime)){
+        if (actualResult.contains(expectedKelime)) {
             System.out.println("Title testi PASSED");
-        }else {
-            System.out.println("Title amazon icermiyor test FAILED");
+        } else {
+            System.out.println("Title amazon icermiyor, test FAILED");
             System.out.println(driver.getTitle());
         }
 
-
-        // gittigimiz sayfanin URL'nin https://amazon.com/ oldugunu test edin
-
         String expectedUrl = "https://www.amazon.com/";
         String actualUrl = driver.getCurrentUrl();
-
-        if (expectedUrl.equals(actualUrl)){
-            System.out.println("URL test PASSED");
-        }else {
-            System.out.println("URL istediginiz degerde degil");
+        if (expectedUrl.equals(actualUrl)) {
+            System.out.println("Url testi PASSED");
+        } else {
+            System.out.println("Url istediginiz degerde degil");
             System.out.println(driver.getCurrentUrl());
         }
 
-
-        Thread.sleep(5000);
+        Thread.sleep(5000L);
         driver.close();
-
     }
 }
---
+++ src/test/java/day04_xpath_cssSelector/C01_XPath.java
@@ -0,0 +1,7 @@
+package day04_xpath_cssSelector;
+
+public class C01_XPath {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", src/driver/c)
+    }
+}
